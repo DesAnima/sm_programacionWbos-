fdfc1d4ea448b57b88a3c9ff3cc24bc69f58ee7c
DesAnima <Destrogames11@yahoo.com>
2023-04-18 11:14:11 -0300

    punto2

---
+++ actividad 2.php
@@ -0,0 +1,13 @@
+<?php
+$numerouno=0;
+$numerodos=1;
+if($numerouno>$numerodos)
+{
+    print("$numerouno es mayor a $numerodos");
+}
+if($numerouno<$numerodos)
+{
+    print("$numerouno es menor a $numerodos");
+}
+
+?>
