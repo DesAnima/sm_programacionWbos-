13863b68b953bac7ae750a76d6a392ba9c13bafb
DesAnima <Destrogames11@yahoo.com>
2023-05-09 08:38:25 -0300

    Punto 5 javaeskrit

---
+++ jabaeskrit.js
@@ -0,0 +1,15 @@
+ let contador = 1
+ let Numero; 
+ while(contador<=5){
+ 
+   
+ Numero = window.prompt("introducir Numero"+contador);
+contador++;
+ }
+ document.write ("Fin del Programa Contador ya no es menor a 5 Último numero introducido:"+Numero);
+
+
+
+
+
+
